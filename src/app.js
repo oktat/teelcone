f77d993af0c414f2640a135ac7301798ffc8b8cc
Sallai András <andrewteki@gmail.com>
2024-12-03 11:39:16 +0100

    Kezdés

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import {calcVolume} from './utils.js';
+
+const doc = {
+  heightInput: document.querySelector('#height'),
+  radius1Input: document.querySelector('#radius1'),
+  radius2Input: document.querySelector('#radius2'),
+  volumeInput: document.querySelector('#volume'),
+  calcButton: document.querySelector('#calcButton'),
+  clearButton: document.querySelector('#clearButton'),
+  aboutButton: document.querySelector('#aboutButton')
+};
+
+doc.calcButton.addEventListener('click', () => {
+  const height = Number(doc.heightInput.value);
+  const radius1 = Number(doc.radius1Input.value);
+  const radius2 = Number(doc.radius2Input.value);
+  const volume = calcVolume(height, radius1, radius2);
+  doc.volumeInput.value = volume;
+});
+
+doc.clearButton.addEventListener('click', () => {
+  doc.heightInput.value = '';
+  doc.radius1Input.value = '';
+  doc.radius2Input.value = '';
+  doc.volumeInput.value = '';
+});
+
+doc.aboutButton.addEventListener('click', () => {
+  window.location.href = 'about.html';
+});
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+
+
+function calcVolume(height, radius1, radius2) {
+  return 1.0/3.0 * Math.PI * radius1 * radius2 * height;
+}
+
+export {calcVolume}
+
